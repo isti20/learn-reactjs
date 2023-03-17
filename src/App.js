6166dabf5c837d4aa024393dbcee35e4cf941ce2
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-18 00:25:37 +0700

    Props in Class Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import FComponent from './components/component/FunctionalComponent';
 import CComponent from './components/component/ClassComponent';
 import ComponentJSX from './components/component/Component';
+import UserClass from './components/props/UserClass';
 
 function App() {
+  const name = "Ngoding Channel"
+  const email = "ngoding@gmail.com"
+  const pengikut = 10000
   return (
     <div className="App">
       <header className="App-header">
-        {/* <FComponent /> */}
+        <UserClass name="ReactJS Channel" email="reactjs@gmail.com" subscriber={5000}/>
+        <UserClass name={name} email={email} subscriber={pengikut}/>
+        {/* <ComponentJSX /> */}
         {/* <CComponent /> */}
-        <ComponentJSX />
+        {/* <FComponent /> */}
       </header>
     </div>
   );
