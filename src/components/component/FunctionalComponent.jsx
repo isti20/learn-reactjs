3c65e96414716bb32fb7ca6099e3f99bfc6b931d
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-18 00:02:25 +0700

    Components: Functional Component

---
+++ src/components/component/FunctionalComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FComponent = () => {
+    const namaChannel = "Learn React JS";
+    const episode = "Functional Component";
+
+    return(
+        <div>
+            <h1>{namaChannel}</h1>
+            <p>{episode}</p>
+        </div>
+    )
+}
+
+export default FComponent
